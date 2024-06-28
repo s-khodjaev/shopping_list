12be8642417f229d3ea6acb9e9a558c1ff7bd93a
skhodjaev <skhodjaev@icloud.com>
2024-06-28 15:12:08 +0500

    prevent duplicate items

--- script.js
+++ script.js
@@ -34,6 +34,11 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove('edit-mode');
     itemToEdit.remove();
     isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert('Item already exists');
+      return;
+    }
   }
 
   // Create item DOM element
@@ -103,6 +108,11 @@ function onClickItem(e) {
   } else {
     setItemToEdit(e.target);
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
